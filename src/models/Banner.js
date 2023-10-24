45cbb1a007dcafe737c567265da6c061f4779e1d
OmarCarrillo1121 <omarcarrillo1121@gmail.com>
2023-10-24 11:36:50 -0600

    Models- Added Banner.js

---
+++ src/models/Banner.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+
+  sequelize.define(
+    "banner",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV1,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    },
+    { timestamps: true }
+  );
+};
