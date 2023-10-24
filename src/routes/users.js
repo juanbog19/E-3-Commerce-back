b6b3cdfb5d140b26a5a48ed3df007fc8c218f796
EmiCastro1997 <casacastro97@gmail.com>
2023-10-24 14:40:18 -0300

    prueba de push

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,7 @@
 const {Router} = require("express");
 const router = Router();
 
+ //esto es de prueba 
 const { getUsers} = require('../controllers/users');
 
 router.get('/users', getUsers); // ruta y (cb) <- con req, res
