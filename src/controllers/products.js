65748ca72c0037a980687b3731def71e768f47d3
EmiCastro1997 <casacastro97@gmail.com>
2023-10-24 21:56:32 -0300

    products actualizado

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -3,6 +3,8 @@ const axios = require("axios");
 const {Op} = require("sequelize");
 
 const STATUS_OK = 200;
+const STATUS_CREATED=201;// Se usa avisar que se ha creado un nuevo recurso en el servidor como resultado de la solicitud. Por ejemplo, se utiliza en respuestas a solicitudes POST exitosas cuando se crea un nuevo objeto en la base de datos.
+const STATUS_NO_CONTENT=204; //Lo uso para indicar que una solicitud se ha procesado joya ;) pero no hay contenido para devolver en la respuesta.
 const STATUS_ERROR = 500;
 
 const getProduct = async (req, res) => {
@@ -13,8 +15,85 @@ const getProduct = async (req, res) => {
     } catch (error) {
         res.status(STATUS_ERROR).end(error.message)
     }
-}
+};
+
+const addProduct = async (req, res) => {
+    try {
+      const {
+        model,
+        memory,
+        storage,
+        cpu,
+        battery,
+        size,
+        special_features,
+      } = req.body;
+      const product = await Product.create({
+        model,
+        memory,
+        storage,
+        cpu,
+        battery,
+        size,
+        special_features,
+      });
+      res.status(STATUS_CREATED).json(product);
+    } catch (error) {
+      res.status(STATUS_ERROR).json({ error: error.message });
+    }
+  };
+  
+  const updateProduct = async (req, res) => {
+    try {
+      const productId = req.params.id;
+      const {
+        model,
+        memory,
+        storage,
+        cpu,
+        battery,
+        size,
+        special_features,
+      } = req.body;
+      const product = await Product.findByPk(productId);
+  
+      if (!product) {
+        res.status(STATUS_ERROR).json({ error: "Producto no encontrado" });
+      } else {
+        product.model = model;
+        product.memory = memory;
+        product.storage = storage;
+        product.cpu = cpu;
+        product.battery = battery;
+        product.size = size;
+        product.special_features = special_features;
+        await product.save();
+        res.status(STATUS_OK).json(product);
+      }
+    } catch (error) {
+      res.status(STATUS_ERROR).json({ error: error.message });
+    }
+  };
+  
+  const deleteProduct = async (req, res) => {
+    try {
+      const productId = req.params.id;
+      const product = await Product.findByPk(productId);
+  
+      if (!product) {
+        res.status(STATUS_ERROR).json({ error: "Producto no encontrado" });
+      } else {
+        await product.destroy();
+        res.status(STATUS_NO_CONTENT).end();
+      }
+    } catch (error) {
+      res.status(STATUS_ERROR).json({ error: error.message });
+    }
+  };
 
 module.exports = {
-    getProduct
+    getProduct,
+    addProduct,
+    updateProduct,
+    deleteProduct,
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,17 @@
 const {Router} = require("express");
 const router = Router();
 
-const {getProduct} = require("../controllers/products");
-
-router.get("/products", getProduct); // ruta y (cb) <- con req, res
+const {
+    getProduct,
+    addProduct,
+    updateProduct,
+    deleteProduct,
+  } = require("../controllers/products");
+  
+  
+  router.get("/products", getProduct); // Obtener todos los productos
+  router.post("/products", addProduct); // Crear un nuevo producto
+  router.put("/products/:id", updateProduct); // Actualizar un producto existente
+  router.delete("/products/:id", deleteProduct); // Eliminar producto
 
 module.exports = router;
