5a3e41acfa4b3d49b52534d21c384bae482c21a0
OmarCarrillo1121 <omarcarrillo1121@gmail.com>
2023-10-25 21:33:23 -0600

    Rutas- Se agrego ruta completa de products con CRUDS completos

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,18 @@
-const {Router} = require("express");
-const router = Router();
+const { Router } = require("express");
+const productsRouter = Router();
 
-const {getProduct} = require("../controllers/products");
+const {
+  getProductsHandler,
+  getProductHandler,
+  postProductHandler,
+  editProductHandler,
+  deleteProductHandler,
+} = require("../handlers/productsHandler");
 
-router.get("/products", getProduct); // ruta y (cb) <- con req, res
+productsRouter.get("/", getProductsHandler); // Obtener todos los productos
+productsRouter.get("/:id", getProductHandler); // Obtener todos los productos
+productsRouter.post("/", postProductHandler); // Crear un nuevo producto
+productsRouter.put("/:id", editProductHandler); // Actualizar un producto existente
+productsRouter.delete("/:id", deleteProductHandler); // Eliminar producto
 
-module.exports = router;
+module.exports = productsRouter;
