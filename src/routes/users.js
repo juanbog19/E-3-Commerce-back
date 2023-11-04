36a9332ba5fd8326e9dd58d70fb89e05dfa1af2c
xmontalvvo <xmontalvvo@gmail.com>
2023-11-04 17:07:55 -0500

    Manejo de borrado logico en usuarios OK

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getUsers, getUserId, postUsers, editUser } = require('../controllers/usersControllers');
+const { getUsers, getUserId, postUsers, editUser, banUser } = require('../controllers/usersControllers');
 
 router.get('/users', getUsers); // ruta y (cb) <- con req, res
 router.get('/users/:id', getUserId);
 router.post('/users', postUsers);
 router.put('/users/:id', editUser);
+router.put('/users/ban/:id', banUser);
 
 module.exports = router;
