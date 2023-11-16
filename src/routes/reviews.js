32bcf9b7f0cbb4260d68c7ec681332393d989ceb
OmarCarrillo1121 <omarcarrillo1121@gmail.com>
2023-11-15 19:15:20 -0600

    Se agrego borrado logico a brands y reviews

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -7,10 +7,12 @@ const {
   postReviewHandler,
   editReviewHandler,
   banReviewHandler,
+  getAllReviewsHandler,
 } = require("../handlers/reviewsHandler");
 
 reviewsRouter.get("/reviews", getReviewsHandler); // ruta y (cb) <- con req, res
 reviewsRouter.get("/reviews/:id", getReviewIdHandler);
+reviewsRouter.get("/reviewsAll", getAllReviewsHandler);
 reviewsRouter.post("/reviews", postReviewHandler);
 reviewsRouter.put("/reviews/:id", editReviewHandler);
 reviewsRouter.delete("/reviews/:id", banReviewHandler);
